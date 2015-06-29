090878e00064433fc9985d842b1600918591f110
Shreyansh <shreyansh.jain@jabong.com>
2015-06-29 12:45:19 +0530

    table layout

--- app/src/main/java/com/example/shreyansh/layouts/SimpleActivity.java
+++ app/src/main/java/com/example/shreyansh/layouts/SimpleActivity.java
@@ -14,26 +14,13 @@ public class SimpleActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.modular_clock_text_include);
+        setContentView(R.layout.table_layout);
         Logger.log("created !!");
-
-        /*Spinner spinner = (Spinner) findViewById(R.id.spinner_day);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.day, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);*/
     }
 
     public void onClickTextView(View v) {
         Toast.makeText(getApplication(), "hello !!", Toast.LENGTH_SHORT).show();
     }
-
-    /*public void changeText(View v) {
-        Logger.log("inside changeText");
-        TextView tv = (TextView) findViewById(v.getId());
-        String text = tv.getText().toString();
-        Logger.log(text);
-    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
